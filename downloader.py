ffb108d14ebd70109e13cf22901788297185c8f3
pedro7 <114467456+pedro7@users.noreply.github.com>
2022-12-26 02:35:55 -0300

    Update downloader.py

--- downloader.py
+++ downloader.py
@@ -1,48 +1,63 @@
-from PyQt6.QtWidgets import QApplication, QComboBox, QFileDialog, QHBoxLayout, QLineEdit, QPushButton, QVBoxLayout, QWidget
+from PyQt6.QtCore import QObject, Qt, QThread, pyqtSignal
+from PyQt6.QtWidgets import QApplication, QComboBox, QFileDialog, QHBoxLayout, QLabel, QLineEdit, QPushButton, QVBoxLayout, QWidget
 from pytube import Playlist, YouTube
 from sys import exit
 
-def download_playlist(url, resolution, path):
-    try:
-        playlist = Playlist(url)
-        for video in playlist.videos:
-            download_video(video.watch_url, resolution, path)
-    except:
-        download_video(url, resolution, path)
-    
-def download_video(url, resolution, path):
-    video = YouTube(url)
-    if resolution == 'Audio Only':
-        video.streams.get_audio_only().download(path)
-    else:
-        video.streams.get_by_resolution(resolution).download(path)
+class DownloadWorker(QObject):
+    progress = pyqtSignal(str)
+    finished = pyqtSignal(str)
 
-class Window(QWidget):
+    def download_playlist(self, url, resolution, path):
+        try:
+            playlist = Playlist(url)
+            for video in playlist.videos:
+                self.download_video(video.watch_url, resolution, path)
+        except:
+            self.download_video(url, resolution, path)
+        self.finished.emit('No downloads queued')
+    
+    def download_video(self, url, resolution, path):
+        video = YouTube(url)
+        self.progress.emit(f'Downloading: {video.title}')
+        if resolution == 'Audio Only':
+            video.streams.get_audio_only().download(path)
+        else:
+            video.streams.get_by_resolution(resolution).download(path)
+
+class Downloader(QWidget):
+    download_requested = pyqtSignal(str, str, str)
+
     def __init__(self):
         super().__init__()
+        self.setup_ui()
 
+    def setup_ui(self):
         self.line = QLineEdit()
         self.line.setPlaceholderText('Paste the playlist/video url')
 
         self.combo = QComboBox()
         self.combo.addItems(['Audio Only', '720p', '480p', '360p', '240p', '144p'])
 
-        button = QPushButton('Download')
-        button.clicked.connect(self.download)
+        self.button = QPushButton('Download')
+        self.button.clicked.connect(self.download)
+
+        self.label = QLabel('No downloads queued')
+        self.label.setAlignment(Qt.AlignmentFlag.AlignCenter)
 
         layout1 = QHBoxLayout()
         layout1.addWidget(self.line)
         layout1.addWidget(self.combo)
 
-        layout2 = QHBoxLayout()
-        layout2.addWidget(button)
+        layout2 = QVBoxLayout()
+        layout2.addWidget(self.button)
+        layout2.addWidget(self.label)
 
         layout = QVBoxLayout()
         layout.addLayout(layout1)
         layout.addLayout(layout2)
 
         self.setWindowTitle('YouTube Downloader')
-        self.setFixedSize(300, 74)
+        self.setFixedSize(400, 96)
         self.setLayout(layout)
         self.show()
 
@@ -50,12 +65,33 @@ class Window(QWidget):
         url = self.line.text()
         resolution = self.combo.currentText()
         path = QFileDialog().getExistingDirectory()
-        if path:
-            download_playlist(url, resolution, path)
+        
+        if not path:
+            return
+
+        self.button.setDisabled(True)
+
+        self.worker = DownloadWorker()
+        self.thread = QThread(parent=self)
+
+        self.worker.moveToThread(self.thread)
+
+        self.download_requested.connect(self.worker.download_playlist)
+        self.worker.progress.connect(self.update_label)
+        self.worker.finished.connect(self.update_label)
+
+        self.thread.start()
+
+        self.download_requested.emit(url, resolution, path)
+
+        self.button.setEnabled(True)
+
+    def update_label(self, text):
+        self.label.setText(text)
 
 def main():
     app = QApplication([])
-    window = Window()
+    downloader = Downloader()
     exit(app.exec())
 
 if __name__ == '__main__':
