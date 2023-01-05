7735c5dd76ecfd9d38cf39da03e9a68e6445b00c
pedro7 <114467456+pedro7@users.noreply.github.com>
2023-01-05 03:28:16 -0300

    Update downloader.py

--- downloader.py
+++ downloader.py
@@ -1,42 +1,52 @@
 from PyQt6.QtCore import QObject, Qt, QThread, pyqtSignal
 from PyQt6.QtWidgets import QApplication, QComboBox, QFileDialog, QHBoxLayout, QLabel, QLineEdit, QPushButton, QVBoxLayout, QWidget
-from pytube import Playlist, YouTube
+from pytube import Channel, Playlist, YouTube
 from sys import exit
 
 class DownloadWorker(QObject):
     progress = pyqtSignal(str)
-    finished = pyqtSignal(str)
+    complete = pyqtSignal(str)
 
-    def download_playlist(self, url, resolution, path):
+    def download(self, url, resolution, format, path):
+        try:
+            self.download_video(url, resolution, format, path)
+        except:
+            self.download_videos(url, resolution, format, path)
+        self.complete.emit('No downloads queued')
+
+    def download_videos(self, url, resolution, format, path):
         try:
             playlist = Playlist(url)
             for video in playlist.videos:
-                self.download_video(video.watch_url, resolution, path)
+                self.download_video(video.watch_url, resolution, format, path)
         except:
-            self.download_video(url, resolution, path)
-        self.finished.emit('No downloads queued')
-    
-    def download_video(self, url, resolution, path):
+            channel = Channel(url)
+            for video in channel.videos:
+                self.download_video(video.watch_url, resolution, format, path)
+
+    def download_video(self, url, resolution, format, path):
         video = YouTube(url)
         self.progress.emit(f'Downloading: {video.title}')
         if resolution == 'Audio Only':
             video.streams.get_audio_only().download(path)
+        elif resolution == 'Highest Resolution':
+            video.streams.get_highest_resolution().download(path)
         else:
-            video.streams.get_by_resolution(resolution).download(path)
+            video.streams.get_lowest_resolution().download(path)
 
 class Downloader(QWidget):
-    download_requested = pyqtSignal(str, str, str)
+    download_requested = pyqtSignal(str, str, str, str)
 
     def __init__(self):
         super().__init__()
-        self.setup_ui()
+        self.create_gui()
 
-    def setup_ui(self):
+    def create_gui(self):
         self.line = QLineEdit()
-        self.line.setPlaceholderText('Paste the playlist/video url')
+        self.line.setPlaceholderText('Paste the video/playlist/channel url')
 
         self.combo = QComboBox()
-        self.combo.addItems(['Audio Only', '720p', '480p', '360p', '240p', '144p'])
+        self.combo.addItems(['Audio Only', 'Highest Resolution', 'Lowest Resolution'])
 
         self.button = QPushButton('Download')
         self.button.clicked.connect(self.download)
@@ -44,28 +54,26 @@ class Downloader(QWidget):
         self.label = QLabel('No downloads queued')
         self.label.setAlignment(Qt.AlignmentFlag.AlignCenter)
 
-        layout1 = QHBoxLayout()
-        layout1.addWidget(self.line)
-        layout1.addWidget(self.combo)
+        top_layout = QHBoxLayout()
+        top_layout.addWidget(self.line)
+        top_layout.addWidget(self.combo)
 
-        layout2 = QVBoxLayout()
-        layout2.addWidget(self.button)
-        layout2.addWidget(self.label)
+        bottom_layout = QVBoxLayout()
+        bottom_layout.addWidget(self.button)
+        bottom_layout.addWidget(self.label)
 
         layout = QVBoxLayout()
-        layout.addLayout(layout1)
-        layout.addLayout(layout2)
+        layout.addLayout(top_layout)
+        layout.addLayout(bottom_layout)
 
         self.setWindowTitle('YouTube Downloader')
-        self.setFixedSize(400, 96)
+        self.setFixedSize(500, 96)
         self.setLayout(layout)
         self.show()
 
     def download(self):
-        url = self.line.text()
-        resolution = self.combo.currentText()
         path = QFileDialog().getExistingDirectory()
-        
+
         if not path:
             return
 
@@ -76,22 +84,25 @@ class Downloader(QWidget):
 
         self.worker.moveToThread(self.thread)
 
-        self.download_requested.connect(self.worker.download_playlist)
+        self.download_requested.connect(self.worker.download)
         self.worker.progress.connect(self.update_label)
-        self.worker.finished.connect(self.update_label)
-        self.worker.finished.connect(lambda: self.button.setEnabled(True))
+        self.worker.complete.connect(self.update_label)
+        self.worker.complete.connect(lambda: self.button.setEnabled(True))
 
         self.thread.start()
 
-        self.download_requested.emit(url, resolution, path)
+        url = self.line.text()
+        resolution = self.combo.currentText()
+        self.download_requested.emit(url, resolution, '', path)
 
     def update_label(self, text):
         self.label.setText(text)
 
-def main():
+def app():
     app = QApplication([])
+    app.setStyle('Fusion')
     downloader = Downloader()
     exit(app.exec())
 
 if __name__ == '__main__':
-    main()
+    app()
